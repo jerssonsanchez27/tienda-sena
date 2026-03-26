8e2a43833f0b52bb3fffd30ce2885756de9058d5
jerssonsanchez27 <jerssonsanchez2007@gmail.com>
2026-03-26 08:31:09 -0500

    ajuste en DetalleVentaRepository

--- src/main/java/com/sena/tienda/repository/DetalleVentaRepository.java
+++ src/main/java/com/sena/tienda/repository/DetalleVentaRepository.java
@@ -1,7 +1,11 @@
 package com.sena.tienda.repository;
 
+import com.sena.tienda.entity.Bicicleta;
 import com.sena.tienda.entity.DetalleVenta;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface DetalleVentaRepository extends JpaRepository<DetalleVenta, Long> {
+    List<DetalleVenta> findByBicicleta(Bicicleta bicicleta);
 }
