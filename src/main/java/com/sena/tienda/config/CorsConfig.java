14a81e013a0e5095d9d668807c461d2b0d4372e9
jerssonsanchez27 <jerssonsanchez2007@gmail.com>
2026-04-01 17:11:41 -0500

    solucione errores despliegue

--- src/main/java/com/sena/tienda/config/CorsConfig.java
+++ src/main/java/com/sena/tienda/config/CorsConfig.java
@@ -1,21 +1,17 @@
 package com.sena.tienda.config;
-import org.springframework.context.annotation.Bean;
+
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
-public class CorsConfig {
-    @Bean
-    public WebMvcConfigurer corsConfigurer() {
-        return new WebMvcConfigurer() {
-            @Override
-            public void addCorsMappings(CorsRegistry registry) {
-                registry.addMapping("/api/**")
-                        .allowedOrigins("http://localhost:4200")
-                        .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
-                        .allowedHeaders("*");
-            }
-        };
+public class CorsConfig implements WebMvcConfigurer {
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/api/**")
+                .allowedOrigins("https://frontend-bici-9u6chy8vy-jerssonsanchez27s-projects.vercel.app")
+                .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
+                .allowedHeaders("*");
     }
 }
